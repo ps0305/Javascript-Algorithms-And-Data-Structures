c8d183eb90b9d55982a3c37e042f5ae35b1f406e
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-13 23:22:53 +0530

    completed

---
+++ Projects/Caesars Cipher/Caesars Cipher.js
@@ -0,0 +1,58 @@
+/*One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.*/
+
+//Basic Code Solution:
+function rot13(str) {
+  // Split str into a character array
+  return str.split('')
+  // Iterate over each character in the array
+    .map.call(str, function(char) {
+      // Convert char to a character code
+      x = char.charCodeAt(0);
+      // Checks if character lies between A-Z
+      if (x < 65 || x > 90) {
+        return String.fromCharCode(x);  // Return un-converted character
+      }
+      //N = ASCII 78, if the character code is less than 78, shift forward 13 places
+      else if (x < 78) {
+        return String.fromCharCode(x + 13);
+      }
+      // Otherwise shift the character 13 places backward
+      return String.fromCharCode(x - 13);
+    }).join('');  // Rejoin the array into a string
+}
+
+
+//Intermediate Code Solution:
+// Solution with Regular expression and Array of ASCII character codes
+function rot13(str) {
+  var rotCharArray = [];
+  var regEx = /[A-Z]/ ;
+  str = str.split("");
+  for (var x in str) {
+    if (regEx.test(str[x])) {
+      // A more general approach
+      // possible because of modular arithmetic
+      // and cyclic nature of rot13 transform
+      rotCharArray.push((str[x].charCodeAt() - 65 + 13) % 26 + 65);
+    } else {
+      rotCharArray.push(str[x].charCodeAt());
+    }
+  }
+  str = String.fromCharCode.apply(String, rotCharArray);
+  return str;
+}
+
+// Change the inputs below to test
+rot13("LBH QVQ VG!");
+
+
+//Advanced Code Solution:
+function rot13(str) { // LBH QVQ VG!
+  return str.replace(/[A-Z]/g, (L) => String.fromCharCode(65 + (L.charCodeAt(0) - 65 + 13) % 26));
+}
---
+++ Projects/Telephone Number Validator/Telephone Number Validator.js
@@ -0,0 +1,18 @@
+/*Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.*/
+
+
+function telephoneCheck(str) {
+   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+   return regex.test(str);
+}
+telephoneCheck("555-555-5555");
