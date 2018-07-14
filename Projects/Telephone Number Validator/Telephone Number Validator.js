dd5ab3a2e9b5d7eb7d576a5ad882fc4aa5a60302
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-14 10:55:58 +0530

    completed

--- Projects/Telephone Number Validator/Telephone Number Validator.js
+++ Projects/Telephone Number Validator/Telephone Number Validator.js
@@ -1,6 +1,7 @@
 /*Return true if the passed string looks like a valid US phone number.
 
-The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+The following are examples of valid formats for US numbers (refer to the tests below for other variants):
 
 555-555-5555
 (555)555-5555
@@ -8,7 +9,11 @@ The user may fill out the form field any way they choose as long as it has the f
 555 555 5555
 5555555555
 1 555 555 5555
-For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.*/
+
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf.
+Your job is to validate or reject the US phone number based on any combination of the formats provided above.
+The area code is required. If the country code is provided, you must confirm that the country code is 1. 
+Return true if the string is a valid US phone number; otherwise return false.*/
 
 
 function telephoneCheck(str) {
@@ -16,3 +21,13 @@ function telephoneCheck(str) {
    return regex.test(str);
 }
 telephoneCheck("555-555-5555");
+
+/*
+^ denotes the beginning of the string (1\s?)? checks allows for a “1” or a "1 " at the beginning.
+
+\d{n} checks for exactly n number of digits so (\(\d{3}\)|\d{3}) checks for three digits that are allowed to be between parenthesis.
+[\s\-]? checks for spaces or dashes between the groups of digits.
+
+$ denotes the end of the string. In this case the beginning and end of the string are used in the regex to prevent it from matching any longer string that might contain a valid phone number (eg. “s 555 555 5555 a”).
+
+Lastly we use regex.test(str) to test if the string adheres to the regular expression and return true or false.*/
