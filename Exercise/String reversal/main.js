5ee0a6b41b5c1420f99dec5be93306449f9941ba
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-16 18:28:05 +0530

    commit

---
+++ Exercise/String reversal/main.js
@@ -0,0 +1,31 @@
+
+// 1. Using built-in functions
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+
+/* 
+2. With for loop
+
+function reverseString(str) {
+  var newString = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+  }
+  return newString;
+}
+
+*/
+
+/* 
+3. With recursive function and ternery operator
+
+function reverseString(str) {
+  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+}
+
+*/
+
