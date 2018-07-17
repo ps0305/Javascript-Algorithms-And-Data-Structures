a30850160c1bccf7d292bcc0e67a12aaae5ad15b
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-17 18:59:55 +0530

    commit

---
+++ ES6/Define a Constructor Function.js
@@ -0,0 +1,39 @@
+/*ES6 provides a new syntax to help create objects, using the keyword class.
+
+This is to be noted, that the class syntax is just a syntax, and not a full-fledged class based implementation of object oriented paradigm, unlike in languages like Java, or Python, or Ruby etc.
+
+In ES5, we usually define a constructor function, and use the new keyword to instantiate an object.
+
+var SpaceShuttle = function(targetPlanet){
+  this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+The class syntax simply replaces the constructor function creation:
+
+class SpaceShuttle {
+  constructor(targetPlanet){
+    this.targetPlanet = targetPlanet;
+  }
+}
+const zeus = new SpaceShuttle('Jupiter');
+Notice that the class keyword declares a new function, and a constructor was added, which would be invoked when new is called - to create a new object.
+
+
+Use class keyword and write a proper constructor to create the Vegetable class.
+
+The Vegetable lets you create a vegetable object, with a property name, to be passed to constructor.*/
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
