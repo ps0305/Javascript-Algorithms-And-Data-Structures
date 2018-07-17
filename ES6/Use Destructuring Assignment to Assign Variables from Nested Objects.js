59c80453e2e397a2f17fb817a49d543d031dd31f
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-17 17:08:39 +0530

    commit

---
+++ ES6/Use Destructuring Assignment to Assign Variables from Nested Objects.js
@@ -0,0 +1,29 @@
+/*We can similarly destructure nested objects into variables.
+
+Consider the following code:
+
+const a = {
+  start: { x: 5, y: 6},
+  end: { x: 6, y: -9 }
+};
+const { start : { x: startX, y: startY }} = a;
+console.log(startX, startY); // 5, 6
+In the example above, the variable start is assigned the value of a.start, which is also an object.
+
+
+Use destructuring assignment to obtain max of forecast.tomorrow and assign it to maxOfTomorrow.*/
+
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  // change code below this line
+  const {tomorrow:{max :maxOfTomorrow}} = forecast; // change this line
+  // change code above this line
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
