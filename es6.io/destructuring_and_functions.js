0ef3b07b938227acc8a63a4408345f9df5bef433
Pankaj Singh <singh.pankaj@live.com>
2018-11-13 20:12:38 +0530

    Create destructuring_and_functions.js

---
+++ es6.io/destructuring_and_functions.js
@@ -0,0 +1,16 @@
+function convertCurrency(amount) {
+    const converted = {
+      USD: amount * 0.76,
+      GPB: amount * 0.53,
+      AUD: amount * 1.01,
+      MEX: amount * 13.30
+    };
+    return converted;
+  }
+
+  function tipCalc({ total = 100, tip = 0.15, tax = 0.13 } = {}) {
+    return total + (tip * total) + (tax * total);
+  }
+
+  const bill = tipCalc({ tip: 0.20, total: 200 });
+  console.log(bill);
