a9ed1dc77e4e520c79b22a0463fa53fd51952b7c
Pankaj Singh <singh.pankaj@live.com>
2022-02-21 23:51:25 +0530

    Update index.js

--- leetcode/binary-search/index.js
+++ leetcode/binary-search/index.js
@@ -3,20 +3,24 @@
  * @param {number} target
  * @return {number}
  */
-const search = function(nums, target) {
-  let i = 0;
-  let j = nums.length - 1;
-  while (i <= j) {
-    let m = Math.floor((i + j) / 2);
-    if (target === nums[m]) {
-      return m;
-    } else if (target > nums[m]) {
-      i = m + 1;
-    } else if (target < nums[m]) {
-      j = m - 1;
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const middle = Math.floor( ((left + right) / 2) );
+        const current = nums[middle];
+        
+        if (current > target) {
+            // ignore the right part from the array and the current index
+            right = middle - 1;
+        } else if (current < target) {
+            // ignore the left part from the array and the current index
+            left = middle + 1
+        } else {
+            return middle;
+        }
     }
-  }
-  return -1;
+    // return -1 if middle did never equal the target
+    return -1;
 };
-
-export default search;
