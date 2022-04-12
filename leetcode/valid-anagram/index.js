6bcbeb06e2b3734379b23f1b7fd6e63cc5e11dc3
Pankaj Singh <singh.pankaj@live.com>
2022-04-12 22:52:51 +0530

    Update index.js

--- leetcode/valid-anagram/index.js
+++ leetcode/valid-anagram/index.js
@@ -4,6 +4,37 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+    
+    let sCharCount = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        let sChar = s[i];
+        sCharCount[sChar] = sCharCount[sChar] + 1 || 1;
+    }
+    
+    for (let i = 0; i < t.length; i++) {
+        const tChar = t[i];
+        if ( !sCharCount[tChar] ) {
+            return false
+        } else {
+            sCharCount[tChar]--
+        }
+    }
+    
+    return true;
+};
+
+// ================================================================================================================================================================
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
   const freq = createFreq(t);
   for (const c of s) {
     freq[c] = (freq[c] || 0) - 1;
