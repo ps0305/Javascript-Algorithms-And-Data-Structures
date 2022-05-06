ce722fac82736b7aace4df5eb07a03559a00d867
Pankaj Singh <singh.pankaj@live.com>
2022-05-06 19:27:58 +0530

    Delete HackerRank-10-Days-Of-JavaScript-Solutions directory

--- HackerRank-10-Days-Of-JavaScript-Solutions/Day 8. A. Create a Button/button.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('btn');
-    
-    button.addEventListener('click', (e) => {
-          const count = Number(e.currentTarget.innerText) + 1;
-        
-          e.currentTarget.innerText = count;
-     });
-});
