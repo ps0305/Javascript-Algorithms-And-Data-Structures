392ad2ed28409cf0ddea0a6a5e2cfd05040bc2e5
Pankaj Singh <singh.pankaj@live.com>
2022-05-21 09:38:35 +0530

    Create polyfills.js

--- Coding Interview Prep/polyfills.js
+++ Coding Interview Prep/polyfills.js
@@ -35,3 +35,27 @@ Array.prototype.reduceAlbums = function(callback, initialValue) {
   }
   return accumulator
 } // our polyfill for reduce
+
+// bind
+
+```js
+let obj = {
+  name: 'Jack',
+};
+
+let myFunc = function (id, city) {
+  console.log(`${this.name}, ${id}, ${city}`);  // id will be undefined
+};
+
+// Accepting any number of arguments passed to myBind
+Function.prototype.myBind = function (obj, ...args) {
+  let func = this;
+  // Accepting arguments passed to newFunc
+  return function (...newArgs) {
+    func.apply(obj, [...args, ...newArgs]);
+  };
+};
+
+let newFunc = myFunc.myBind(obj, 'a_random_id')
+newFunc('New York') // Jack, a_random_id, New York
+```
